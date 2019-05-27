866825fd905d94f68e25f201e4b80a4501859d99
sailingzhang <aa582885848@outlook.com>
2019-05-28 00:23:05 +0800

    test wrapper

--- python/gym_tes.py
+++ python/gym_tes.py
@@ -2,6 +2,7 @@
 from log_init import log_init
 import logging
 import gym
+import random
 
 
 def gym_test1():
@@ -14,9 +15,35 @@ def gym_test1():
     env.close()
 
 
-class wa
+class RandomWrapper(gym.ActionWrapper):
+    def __init__(self,env,espilon=0.5):
+        super().__init__(env)
+        self.espilon = espilon
+    def action(self,action):
+        if random.random() < self.espilon:
+            logging.debug("Random")
+            return self.env.action_space.sample()
+        else:
+            logging.debug("no Random")
+            return action
+
+def mywrapperTest():
+    env = RandomWrapper(gym.make('CartPole-v0'))
+    obs = env.reset()
+    total_reward = 0.0
+    while True:
+        env.render()
+        obs,reward,done,_ = env.step(0)
+        if done:
+            # pass
+            break
+    logging.debug("reward get={}".format(total_reward))
+
+
+
 
 
 if __name__ == '__main__':
     log_init("gymtest.log")
-    gym_test1()
+    # gym_test1()
+    mywrapperTest()
