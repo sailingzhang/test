bd9d6b32c94b9f840484dbf5e78394562945d108
sailingzhang <aa582885848@outlook.com>
2019-05-28 08:34:09 +0800

    add pytorch

--- python/gym_tes.py
+++ python/gym_tes.py
@@ -3,6 +3,8 @@ from log_init import log_init
 import logging
 import gym
 import random
+import torch
+import torch.nn as nn
 
 
 def gym_test1():
@@ -40,10 +42,28 @@ def mywrapperTest():
     logging.debug("reward get={}".format(total_reward))
 
 
+def torchGradientTest():
+    v1 = torch.tensor([4.0,1.0],requires_grad = True)
+    v2 = torch.tensor([2.0,2.0])
+    v_sum = v1+v2
+    v_res = (v_sum*2).sum()
+    v_res.backward()
+    logging.debug("v_res={},v1.grad={}".format(v_res,v1.grad))
 
+def nntest():
+    l = nn.Linear(2,5)
+    v = torch.FloatTensor([1,2])
+    logging.debug("l(v)={}".format(l(v)))
+    s = nn.Sequential(
+        nn.Linear(2,5),
+        nn.ReLU()
+    )
 
+    logging.debug("s={}".format(s(v)))
 
 if __name__ == '__main__':
     log_init("gymtest.log")
     # gym_test1()
-    mywrapperTest()
+    # mywrapperTest()
+    # torchGradientTest()
+    nntest()
