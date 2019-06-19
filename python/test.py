73b4a8474713334db76ad7c84eeb2171d227e5eb
sailingzhang <aa582885848@outlook.com>
2019-06-19 08:30:09 +0800

    add some test

--- python/test.py
+++ python/test.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import logging
+import torch
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 sys.path.append("..")
 from log_init import log_init
@@ -26,7 +27,30 @@ def yieldtest():
         logging.debug("j={}".format(j))
 
 
+def gatherTest():
+    t1 = torch.Tensor([[1,2,3],[4,5,6]])
+    logging.debug("t1.size={}".format(t1.size()))
+    logging.debug("t1={}".format(t1))
+    newt1 = torch.gather(t1,dim=1,index=torch.LongTensor([[0,1],[2,0]]))
+    logging.debug("newt1={}".format(newt1))
+
+    newt2 =torch.gather(t1,dim=0,index=torch.LongTensor([[0,1,1],[0,0,0]]))
+    logging.debug("newt2={}".format(newt2))
+
+
+def squeezeTest():
+    sq1 =torch.Tensor([[[1,2,3]],[[4,5,6]]])
+    logging.debug("sq1'size={}".format(sq1.size()))
+    asq2 = sq1.squeeze(1)
+    logging.debug("asq2={}".format(asq2))
+    asq3 = asq2.unsqueeze(2)
+    logging.debug("asq3={}".format(asq3))
+    
+	
+
 if __name__ == "__main__":
     log_init("test.log")
     # filterTest()
-    yieldtest()
+    # yieldtest()
+    # gatherTest()
+    squeezeTest()
